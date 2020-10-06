4e7244acb28373292098e61429a8d2adb7827c35
Fernando <fer_char98@hotmail.com>
2020-10-05 22:50:35 -0500

    Navegacion entre las ventanas

--- app/src/main/java/com/example/proyecto_mobiles/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_mobiles/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.proyecto_mobiles
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnRegistrarse : TextView = findViewById(R.id.txt_Registrate)
+        btnRegistrarse.setOnClickListener {
+            val intent = Intent(this, usuario_registro::class.java)
+            startActivity(intent)
+        }
     }
+
 }
