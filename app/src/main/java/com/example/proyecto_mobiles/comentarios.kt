a2ff96476fd3a2664d660c01fb0bf499299ff58b
Juan <juanchowooi@gmail.com>
2020-10-29 01:09:50 -0600

    Configuracion de usuario, favoritos y comentarios

---
+++ app/src/main/java/com/example/proyecto_mobiles/comentarios.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_mobiles
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class comentarios : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comentarios)
+    }
+}
