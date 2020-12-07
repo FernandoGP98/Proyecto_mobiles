b93c906319f56b658a748bce14f9bb3bd0b4bf3a
Fernando <fer_char98@hotmail.com>
2020-12-07 14:37:16 -0600

    Update password

--- app/src/main/java/com/example/proyecto_mobiles/perfil_configuracion.kt
+++ app/src/main/java/com/example/proyecto_mobiles/perfil_configuracion.kt
@@ -168,17 +168,47 @@ class perfil_configuracion : AppCompatActivity() {
                     alert.setTitle("ERROR")
                     alert.show()
                 } else {
-                    val alertDialog3 =
-                        AlertDialog.Builder(this, R.style.Alert)
-                    alertDialog3.setMessage("Contraseña Actualizada")
-                        .setCancelable(false)
-                        .setNegativeButton("OK", DialogInterface.OnClickListener { dialog, id ->
-                            dialog.cancel()
-                        })
-                    val alert = alertDialog3.create()
-                    alert.setTitle("EXITO")
-                    alert.show()
                     contrasenaFinalNueva = contra1N
+
+                    val queue = Volley.newRequestQueue(this)
+                    val body = JSONObject()
+                    body.put("password", contrasenaFinalNueva)
+                    body.put("id", ses.getID())
+
+                    val requ = JsonObjectRequest(
+                        Request.Method.POST,
+                        "https://restaurantespia.herokuapp.com/UsuarioUpdatePass",
+                        body,
+                        { response: JSONObject? ->
+                            val success = response?.getInt("success")
+                            if(success==1) {
+                                val usuario = response.getJSONObject("usuario")
+                                ses.savePass(usuario.getString("password"))
+
+                                val toast = Toast.makeText(this, "Contraseña Actualizado", Toast.LENGTH_LONG)
+                                toast.show()
+                            }else{
+                                val toast = Toast.makeText(this, "Error de actualizacion", Toast.LENGTH_LONG)
+                                toast.show()
+                            }
+
+                        },
+                        { error ->
+                            error.printStackTrace()
+                            Log.e("Servicio web", "Web", error)
+                            if (error.toString() == "com.android.volley.ServerError") {
+                                val toast = Toast.makeText(this, "Error del servidor", Toast.LENGTH_LONG)
+                                toast.show()
+                            }
+                        })
+                    requ.setRetryPolicy(
+                        DefaultRetryPolicy(
+                            5000,
+                            DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                            DefaultRetryPolicy.DEFAULT_BACKOFF_MULT
+                        )
+                    )
+                    queue.add(requ)
                 }
             }
         }
